40ae60cbf4426cc872c2368a788a324d72056271
masayuki <masatajima12@gmail.com>
2024-12-12 00:05:08 +0900

    signsテーブルとweathersテーブルを作成

---
+++ database/migrations/2024_12_09_195438_create_weathers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('weathers', function (Blueprint $table) {
+            $table->id();
+            $table->string('weather')->comment('天気の情報');
+            $table->bigInteger('temperature')->comment('気温');
+            $table->bigInteger('humidity')->comment('湿度');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('weathers');
+    }
+};
