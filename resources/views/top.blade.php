0d4de581916ff98afb41b1914b187d8c3a3add38
masayuki <masatajima12@gmail.com>
2024-12-12 23:39:20 +0900

    トップページのヘッダーを仮作成

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>体調管理アプリ</title>
-</head>
-<body>
+<x-top-layout>
     <h1>ユーザートップページです</h1>
-</body>
-</html>
+</x-top-layout>
