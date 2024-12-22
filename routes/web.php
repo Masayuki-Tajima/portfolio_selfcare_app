60f2f2bb0ee78de2a98202070a08f52eff655416
masayuki <masatajima12@gmail.com>
2024-12-22 16:15:27 +0900

    ユーザーのログアウト機能を実装

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ConditionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('top');
 });
 
 Route::get('/dashboard', function () {
