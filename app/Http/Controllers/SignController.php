690a056e40841389318560e80fce15a63fb4d6bc
masayuki <masatajima12@gmail.com>
2024-12-29 21:32:42 +0900

    体調サインのコントローラーを作成

---
+++ app/Http/Controllers/SignController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SignController extends Controller
+{
+    public function index(){
+        $signs = Auth::user()->signs;
+
+        return view('signs.index', [
+            'signs' => $signs
+        ]);
+    }
+}
