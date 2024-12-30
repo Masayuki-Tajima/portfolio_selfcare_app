53c4b15fa60d49e6560d2a00acab874ec5f87d06
masayuki <masatajima12@gmail.com>
2024-12-31 00:29:57 +0900

    weatherテーブルに新規データを追加

--- database/seeders/WeatherSeeder.php
+++ database/seeders/WeatherSeeder.php
@@ -13,14 +13,24 @@ class WeatherSeeder extends Seeder
      */
     public function run(): void
     {
+        // DB::table('weathers')->insert([
+        //     'weather' => '快晴',
+        //     'condition_id' => 1,
+        //     'temperature' => 12,
+        //     'humidity' => 50,
+        //     'created_at' => '2024-12-23 00:00:00',
+        //     'updated_at' => '2024-12-23 00:00:00',
+        // ]);
+
         DB::table('weathers')->insert([
             'weather' => '快晴',
-            'condition_id' => 1,
-            'temperature' => 12,
-            'humidity' => 50,
-            'created_at' => '2024-12-23 00:00:00',
-            'updated_at' => '2024-12-23 00:00:00',
+            'condition_id' => 2,
+            'temperature' => 15,
+            'humidity' => 40,
+            'created_at' => '2024-12-29 00:00:00',
+            'updated_at' => '2024-12-29 00:00:00',
         ]);
+
 
     }
 }
