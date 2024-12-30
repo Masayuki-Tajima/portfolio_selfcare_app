5674be73f16ae5cb054cde54a1ec0d7628e426c3
masayuki <masatajima12@gmail.com>
2024-12-31 00:32:06 +0900

    体調一覧の表示を修正

--- app/Http/Controllers/ConditionController.php
+++ app/Http/Controllers/ConditionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Condition;
 use Illuminate\Http\Request;
+use App\Http\Requests\ConditionRequest;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Weather;
@@ -20,9 +21,10 @@ class ConditionController extends Controller
     public function index($user_id)
     {
         $conditions = Auth::user()->conditions;
-        $signs = Auth::user()->signs->where('user_id', '=', $user_id);
-        // $signs = Condition::findOrFail($user_id)->signs;
-        // dd(Auth::user()->signs->where('user_id', '=', $user_id));
+        // $signs = Auth::user()->signs->where('user_id', '=', $user_id);
+        // dd(Condition::findOrFail($user_id)->signs);
+        $signs = Condition::findOrFail($user_id)->signs;
+        dd(Condition::where('user_id', '=', $user_id)->with('signs')->get());
 
         return view('conditions.index', [
             'conditions' => $conditions,
@@ -31,11 +33,46 @@ class ConditionController extends Controller
     }
 
     //体調の新規登録画面表示
-    public function create($user_id){
-        $signs = Condition::findOrFail($user_id)->signs;
+    public function create($user_id)
+    {
+        $signs = Auth::user()->signs->where('user_id', '=', $user_id);
 
         return view('conditions.add', [
             'signs' => $signs
         ]);
+    }
+
+    public function store(ConditionRequest $request, $user_id)
+    {
+        //conditionテーブルへの値の挿入
+        $condition = new Condition();
+        $condition->user_id = $user_id;
+        $condition->date = $request->input('date');
+        $condition->sleep_time = $request->input('sleep_time');
+        $condition->wakeup_time = $request->input('wakeup_time');
+        $condition->exercise = $request->input('exercise');
+        $condition->breakfast = $request->input('breakfast');
+        $condition->lunch = $request->input('lunch');
+        $condition->dinner = $request->input('dinner');
+        $condition->comment = $request->input('comment');
+        $condition->sleep_duration = $request->input('sleep_duration');
+
+        $condition->save();
+
+        //condition_signテーブルへの値の挿入
+        $condition->signs()->attach($request->input('good_signs'));
+        $condition->signs()->attach($request->input('caution_signs'));
+        $condition->signs()->attach($request->input('bad_signs'));
+
+        //weatherテーブルへの値の挿入
+        $weather = new Weather();
+        $weather->weather = '曇り';
+        $weather->temperature = 26;
+        $weather->humidity = 60;
+
+        $weather->save();
+
+        return redirect()->route('conditions.index', ['user_id' => $user_id]);
+
     }
 }
