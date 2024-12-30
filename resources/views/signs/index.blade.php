8b6f8187e9acb2de0ab892b4d1979b5e0269074e
masayuki <masatajima12@gmail.com>
2024-12-30 12:59:07 +0900

    体調サインの論理削除機能を実装

--- resources/views/signs/index.blade.php
+++ resources/views/signs/index.blade.php
@@ -2,7 +2,7 @@
     <section class="body-font text-gray-600">
         <div class="container mx-auto px-5 py-24">
             {{-- メッセージ欄 --}}
-            <div>
+            <div class="mb-20 flex w-full flex-col text-center text-red-500">
                 @if (session('flash_message'))
                     <p>{{ session('flash_message') }}</p>
                 @endif
@@ -14,15 +14,25 @@
             </div>
             {{-- 新規登録ボタン --}}
             <button type="button"
+                class="mx-auto flex rounded border-0 bg-indigo-500 px-8 py-2 text-lg text-white hover:bg-indigo-600 focus:outline-none"
                 onclick="location.href='{{ route('signs.create', ['user_id' => Auth::id()]) }}' ">体調を新規登録</button>
             <div class="mx-auto w-full overflow-auto lg:w-2/3">
-                {{-- 良好サイン --}}
-                <h2>良好サイン</h2>
+                <h2 class="text-2xl">良好サイン</h2>
                 <ul>
                     @foreach ($signs as $sign)
                         @if ($sign->sign_type == 0)
-                            <li class="px-4 py-3">{{ $sign->sign_type == 0 ? $sign->sign : '無' }}<input name="plan"
-                                    type="radio"></li>
+                            <li class="px-4 py-3">
+                                <p class="inline">{{ $sign->sign }}</p>
+                                <form class="inline"
+                                    action="{{ route('signs.destroy', ['user_id' => Auth::id(), 'sign_id' => $sign->id]) }}"
+                                    method="POST" onsubmit="return confirm('本当に削除してもよろしいですか？');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button
+                                        class="rounded border-0 bg-red-500 px-8 py-2 text-lg text-white hover:bg-red-600 focus:outline-none"
+                                        type="submit">削除</button>
+                                </form>
+                            </li>
                         @endif
                     @endforeach
                 </ul>
@@ -47,24 +57,42 @@
                     </tbody>
                 </table> --}}
 
-                {{-- 注意サイン --}}
-                <h2>注意サイン</h2>
+                <h2 class="text-2xl">注意サイン</h2>
                 <ul>
                     @foreach ($signs as $sign)
                         @if ($sign->sign_type == 1)
-                            <li class="px-4 py-3">{{ $sign->sign_type == 1 ? $sign->sign : '無' }}<input name="plan"
-                                    type="radio"></li>
+                            <li class="px-4 py-3">
+                                <p class="inline">{{ $sign->sign }}</p>
+                                <form class="inline"
+                                    action="{{ route('signs.destroy', ['user_id' => Auth::id(), 'sign_id' => $sign->id]) }}"
+                                    method="POST" onsubmit="return confirm('本当に削除してもよろしいですか？');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button
+                                        class="rounded border-0 bg-red-500 px-8 py-2 text-lg text-white hover:bg-red-600 focus:outline-none"
+                                        type="submit">削除</button>
+                                </form>
+                            </li>
                         @endif
                     @endforeach
                 </ul>
 
-                {{-- 悪化サイン --}}
-                <h2>悪化サイン</h2>
+                <h2 class="text-2xl">悪化サイン</h2>
                 <ul>
                     @foreach ($signs as $sign)
                         @if ($sign->sign_type == 2)
-                            <li class="px-4 py-3">{{ $sign->sign_type == 2 ? $sign->sign : '無' }}<input name="plan"
-                                    type="radio"></li>
+                            <li class="px-4 py-3">
+                                <p class="inline">{{ $sign->sign }}</p>
+                                <form class="inline"
+                                    action="{{ route('signs.destroy', ['user_id' => Auth::id(), 'sign_id' => $sign->id]) }}"
+                                    method="POST" onsubmit="return confirm('本当に削除してもよろしいですか？');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button
+                                        class="rounded border-0 bg-red-500 px-8 py-2 text-lg text-white hover:bg-red-600 focus:outline-none"
+                                        type="submit">削除</button>
+                                </form>
+                            </li>
                         @endif
                     @endforeach
                 </ul>
