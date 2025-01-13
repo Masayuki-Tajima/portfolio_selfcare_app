6206efdb599c78efdb6948a4f630c5c1d3112745
masayuki <masatajima12@gmail.com>
2025-01-13 18:52:44 +0900

    [fix]ログイン前トップページのデザイン修正 #50
    「ログイン画面へ」と「ゲストユーザーで使ってみる」ボタンのレスポンシブデザインを修正しました。

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -63,7 +63,7 @@
                 </div>
             </div>
             <div class="flex flex-wrap -m-4">
-                <div class="xl:w-1/2 md:w-1/2 p-4">
+                <div class="xl:w-1/2 md:w-1/2 p-4 w-full">
                     <div class="border border-gray-200 p-6 rounded-lg">
                         <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
