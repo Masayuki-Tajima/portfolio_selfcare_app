d32f4cd5e2439767efe53ad8aa9111ef9ba78c73
masayuki <masatajima12@gmail.com>
2025-01-18 21:41:06 +0900

    [update]サブタイトルを修正 #52
    「トップページ」直上のh2タグのサブタイトルを修正しました。

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,7 +2,7 @@
     <section class="body-font text-gray-600">
         <div class="container mx-auto px-5 py-24">
             <div class="flex flex-col text-center w-full mb-20">
-                <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
+                <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Health Management App</h2>
                 <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">トップページ</h1>
                 <p class="lg:w-2/3 mx-auto leading-relaxed text-base">ようこそ、体調管理アプリへ！</p>
             </div>
