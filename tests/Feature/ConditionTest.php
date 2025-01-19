02912de0b9ba1a28f86922970f1dbd9da90f2555
masayuki <masatajima12@gmail.com>
2025-01-19 22:20:04 +0900

    [feature]「体調の新規登録画面の表示テスト」を作成 #59

--- tests/Feature/ConditionTest.php
+++ tests/Feature/ConditionTest.php
@@ -15,15 +15,26 @@ class ConditionTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * A basic feature test example.
+     * 体調の新規登録画面の表示テスト
      */
-    public function test_example(): void
+    public function test_the_condition_create_page_display_successful()
     {
-        $response = $this->get('/');
+        //ユーザーデータの用意
+        $user = new User();
+        $user->id = 1;
+        $user->name = 'Yamada';
+        $user->email = 'yamada_test@example.com';
+        $user->password = 'passya123';
+        $user->save();
+
+        $response = $this->actingAs($user)->get(route('conditions.create', ['user_id' => $user->id]));
 
         $response->assertStatus(200);
     }
 
+    /**
+     * 体調の新規登録機能テスト
+     */
     public function test_condition_can_be_created()
     {
         //外部キー制約を無効化
