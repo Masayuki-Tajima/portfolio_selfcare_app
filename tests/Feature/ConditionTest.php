2f8784f8d3eebc4c0666c5d1e5ec9cc400295bc7
masayuki <masatajima12@gmail.com>
2025-01-20 22:51:10 +0900

    [feature]体調の削除機能テストを作成 #59

--- tests/Feature/ConditionTest.php
+++ tests/Feature/ConditionTest.php
@@ -140,4 +140,38 @@ class ConditionTest extends TestCase
         Schema::enableForeignKeyConstraints();
     }
 
+    /**
+     * 体調の削除機能テスト
+     */
+    public function test_condition_can_be_deleted()
+    {
+        //外部キー制約を無効化
+        Schema::disableForeignKeyConstraints();
+
+        //ユーザーデータの用意
+        $user = User::factory()->create();
+
+        //体調データの用意
+        $condition = new Condition();
+        $condition->id = 1;
+        $condition->user_id = $user->id;
+        $condition->date = now()->format('Y-m-d');
+        $condition->sleep_time = '2024-01-08 00:30:00';
+        $condition->wakeup_time = '2024-01-08 08:30:00';
+        $condition->exercise = 'スクワット10回';
+        $condition->breakfast = 'ヨーグルト、バナナ';
+        $condition->lunch = 'ラーメン';
+        $condition->dinner = 'ぶりの照り焼き';
+        $condition->comment = '一日中眠気に襲われた。';
+        $condition->sleep_duration = '08:00:00';
+        $condition->save();
+
+        $this->actingAs($user)->delete(route('conditions.destroy', ['user_id' => $user->id, 'condition_id' => $condition->id]));
+
+        //保存したデータがconditionsテーブルから削除されているか検証
+        $this->assertDatabaseMissing('conditions', ['id' => $condition->id]);
+
+        //外部キー制約を有効化
+        Schema::enableForeignKeyConstraints();
+    }
 }
