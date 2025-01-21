e7675099a905ec85b452bd052eaf1ee5fc6f1bbe
masayuki <masatajima12@gmail.com>
2025-01-21 20:47:32 +0900

    [feature]体調サインの新規登録機能テストを作成 #59

--- tests/Feature/SignTest.php
+++ tests/Feature/SignTest.php
@@ -26,4 +26,31 @@ class SignTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     * 体調サインの新規登録機能テスト
+     */
+    public function test_sign_can_be_created()
+    {
+        //外部キー制約を無効化
+        Schema::disableForeignKeyConstraints();
+
+        //ユーザーデータの用意
+        $user = User::factory()->create();
+
+        //体調サインのデータをPOSTメソッドで送信
+        $sign = [
+            'user_id' => $user->id,
+            'sign' => '咳が出る',
+            'sign_type' => 1,
+        ];
+        $this->actingAs($user)->post(route('signs.store', ['user_id' => $user->id]), $sign);
+
+        //送信したデータがconditionsテーブルに保存されているか検証
+        $this->assertDatabaseHas('signs', $sign);
+
+        //外部キー制約を有効化
+        Schema::enableForeignKeyConstraints();
+    }
+
 }
